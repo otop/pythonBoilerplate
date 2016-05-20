425f9b988b2ee2205fda5eeb67c46a982d600526
Volodymyr Holubets <volodymyr@yallo.com>
2016-05-20 17:46:35 +0300

    added check for max complexity

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+MAX_COMPLEXITY="B"
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/env/bin/activate
 export PYTHONPATH=${PYTHONPATH}:${DIR}
 nosetests . --config=nose.cfg
 
 radon cc -i "env,tests" --order=SCORE -s --xml . > complexity.xml
+
+radon cc -i "env,tests" --order=ALPHA -s -n=${MAX_COMPLEXITY} . 1>&2
 
 flake8 . --output-file flake8.txt
 
