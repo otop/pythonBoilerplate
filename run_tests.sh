204b0b6af536a622c5cefd65e11cda12b4faada6
Volodymyr Holubets <volodymyr@yallo.com>
2016-05-24 14:19:43 +0300

    added html report for flake

--- run_tests.sh
+++ run_tests.sh
@@ -12,24 +12,25 @@ source ${DIR}/env/bin/activate
 echo -e "Export environ vars..."
 export PYTHONPATH=${PYTHONPATH}:${DIR}
 
-echo -e "Run tests..."
-nosetests . --config=nose.cfg
-
 echo -e "Compute CC..."
 radon cc -i ${SKIP_DIRS} --order=SCORE -s --xml . > complexity.xml
 
-echo -e "Check CC"
-radon cc -i ${SKIP_DIRS} --order=ALPHA -s -n=${MAX_COMPLEXITY} . 1>&2
-
-if [[ $(radon cc -i ${SKIP_DIRS} --order=ALPHA -s -n=${MAX_COMPLEXITY} . ) ]]; then
-    exit 1
-fi
-
 echo -e "Run flake8..."
 flake8 . --output-file flake8.txt || true       # continue execution if failed
-echo -e "Create flake8 xml file"
-flake8_junit flake8.txt flake8_junit.xml
+
+echo -e "Create flake8 html file"
+pepper8 -o flake_report.html flake8.txt
 rm flake8.txt
+
+echo -e "Run tests..."
+nosetests . --config=nose.cfg
+
+echo -e "Check CC"
+#radon cc -i ${SKIP_DIRS} --order=ALPHA -s -n=${MAX_COMPLEXITY} . 1>&2
+if [[ $(radon cc -i ${SKIP_DIRS} --order=ALPHA -s -n=${MAX_COMPLEXITY} . ) ]]; then
+    echo -e "Max complexity error!!!"
+    exit 1
+fi
 
 echo -e "\nCheck flake8..."
 flake8 --statistics --count -qq 1>&2
